c8d018d7ea4a571a31e0bf7383c146a97b6bc02e
Volobuev-LI <H9ojnyZs>
2021-09-10 14:51:14 +0500

    Задание 1

---
+++ src/Task/Task1.java
@@ -0,0 +1,16 @@
+package Task;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        // Написать цикл while который десять раз печатет слово HEllo
+
+        // Написать цикл for который десять раз печатет слово HEllo
+
+        // Написать цикл while(true) который напчетает 10 раз слово hello и завершится
+
+
+
+    }
+
+}
