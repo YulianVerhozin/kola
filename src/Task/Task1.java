7c534512fd795a3f9aa7391f6ccde4441df1fb43
yliam <yliamn@mail.ru>
2021-09-10 15:22:31 +0500

    Сделал два задания на третьем задании остановился нужно напомнить

--- src/Task/Task1.java
+++ src/Task/Task1.java
@@ -5,9 +5,22 @@ public class Task1 {
 
         // Написать цикл while который десять раз печатет слово HEllo
 
+        int i = 0;
+        while(i<10) {
+            System.out.println("Hello");
+            i = i + 1;
+        }
+
         // Написать цикл for который десять раз печатет слово HEllo
 
+        for(int q = 0;q<=10;q++){
+            System.out.println("Hello");            // q++ = q
+        }
+
+
         // Написать цикл while(true) который напчетает 10 раз слово hello и завершится
+
+
 
 
 
