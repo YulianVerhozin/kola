03fd0f09b534a88d2c4fd2ac5f6be69ecd4e2df5
yliam <yliamn@mail.ru>
2021-09-23 16:10:12 +0500

    Сделал основную часть

--- src/Main/Task1/Main.java
+++ src/Main/Task1/Main.java
@@ -2,12 +2,23 @@ package Main.Task1;
 
 public class Main {
     public static void main(String[] args) {
-        Phone xiaomi = new Phone("Mi 6", 123, 456);
+        Phone xiaomi = new Phone("Mi 6",456);
 
         System.out.println(xiaomi.model);
         System.out.println(xiaomi.number);
         System.out.println(xiaomi.weight);
 
+         xiaomi.receiveCall("Вася");
 
+
+         int rec = xiaomi.getNumber();
+        System.out.println(rec);
+
+        Phone xiaomi1 = new Phone("Poco");
+
+        Phone xiaomi2 = new Phone();
+        xiaomi2.model = "Mi 10";
+        xiaomi2.number = 4588;
+        xiaomi2.weight = 321;
     }
 }
--- src/Main/Task1/Phone.java
+++ src/Main/Task1/Phone.java
@@ -5,20 +5,26 @@ public class Phone {
     int number;
     int weight;
 
+    public Phone(String model, int number){
+        this.model = model;
+        this.number = number;
+    }
+    public Phone(String model){
+        this.model = model;
+    }
+    public Phone(){
+
+    }
+
     public Phone(String model, int number, int weight){
         this.model = model;
         this.weight = weight;
         this.number = number;
     }
-
-    public static void Main (String[] args){
-        Phone Xiaomi = new Phone("Mi 6", 123, 456);
-
-        Xiaomi.model = "Mi 6";
-        Xiaomi.number = 123;
-        Xiaomi.weight = 456;
-        System.out.println(Xiaomi.model);
-        System.out.println(Xiaomi.number);
-        System.out.println(Xiaomi.weight);
+    public void receiveCall (String name){
+        System.out.println("Звонит " + name);
+    }
+    public int getNumber(){
+        return number;
     }
 }
