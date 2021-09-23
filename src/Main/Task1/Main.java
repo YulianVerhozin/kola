b63c9e7b05e08777dc1aed92696ab3eec25ae2cc
Volobuev-LI <H9ojnyZs>
2021-09-23 14:30:42 +0500

    fix

---
+++ src/Main/Task1/Main.java
@@ -0,0 +1,13 @@
+package Main.Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Phone xiaomi = new Phone("Mi 6", 123, 456);
+
+        System.out.println(xiaomi.model);
+        System.out.println(xiaomi.number);
+        System.out.println(xiaomi.weight);
+
+
+    }
+}
